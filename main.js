2cad0a4f58ee0a661e1c384c9fa0e5f45a33871f
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-05-16 16:27:24 +0200

    tolle wettersymbole

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ async function showForecast(url) {
         {pointToLayer: function(feature, latlng){
             let details = feature.properties.timeseries[0].data.instant.details;
             let time = new Date(feature.properties.timeseries[0].time);
-            console.log(time)
+
             let content =`
             <h4>Wettervorhersage für ${time.toLocaleString()}</h4>
             <ul>
@@ -53,7 +53,11 @@ async function showForecast(url) {
                 <li>Windrichtung (°): ${details.wind_from_direction}</li>
                 <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed*3.6)}</li>
             </ul>  
-                `
+                `;
+            for (let i = 0; i <= 24; i+=3) {
+                let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+                content +=`<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px">`;
+            }
             L.popup(latlng, {content: content})
             .openOn(themaLayer.forecast)
         }}
